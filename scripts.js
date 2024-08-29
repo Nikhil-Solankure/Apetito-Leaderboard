1967b9d88fffaa2a919a3afcdd8b66848764bb00
Nikhil-Solankure <nikhil.solankure@apetito.co.uk>
2024-08-29 10:57:15 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const SHEET_ID = '1b5yydhYkEhzbcZ_4EQcawn5zDbZR5wVeS0D9jBc28DM'; // Replace with your actual Google Sheets ID
 const API_KEY = 'AIzaSyDXTOdt7j5NzrZXhnOIfmWTh-92QY2bUOk'; // Replace with your actual API key
-const SHEET_NAMES = ['Pelaton-Sprint', 'Packaging-Game', 'Card Castle', 'Dementia Chopsticks', 'Hoopla'];
+const SHEET_NAMES = ['Pelaton-Sprint', 'Packaging-Game', 'Bean-Bag-Game', 'Dementia Chopsticks', 'Hoopla'];
 
 let currentGameIndex = 0;
 
@@ -30,15 +30,15 @@ function updateLeaderboard(data) {
 
     $('.rank.gold .player-name').text(sortedRows[0][0]);
     $('.rank.gold .department').text(sortedRows[0][1]);
-    $('.rank.gold .distance').text(`${sortedRows[0][2]}`);
+   // $('.rank.gold .distance').text(`${sortedRows[0][2]}`);
 
     $('.rank.silver .player-name').text(sortedRows[1][0]);
     $('.rank.silver .department').text(sortedRows[1][1]);
-    $('.rank.silver .distance').text(`${sortedRows[1][2]}`);
+  //  $('.rank.silver .distance').text(`${sortedRows[1][2]}`);
 
     $('.rank.bronze .player-name').text(sortedRows[2][0]);
     $('.rank.bronze .department').text(sortedRows[2][1]);
-    $('.rank.bronze .distance').text(`${sortedRows[2][2]}`);
+  //  $('.rank.bronze .distance').text(`${sortedRows[2][2]}`);
 }
 
 function cycleGames() {
